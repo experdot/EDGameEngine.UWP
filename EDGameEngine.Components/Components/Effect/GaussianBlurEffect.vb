7b41c53d6378df637dc6433435a31f40f2b09b49
experdot <1143116306@qq.com>
2016-11-01 17:42:32 +0800

    添加高斯模糊效果器添加公共静态方法

--- EDGameEngine.Components/Components/Effect/GaussianBlurEffect.vb
+++ EDGameEngine.Components/Components/Effect/GaussianBlurEffect.vb
@@ -23,4 +23,18 @@ Public Class GaussianBlurEffect
             Return CanvasBitmap.CreateFromColors(resourceCreator, cac.GetPixelColors, CInt(sizepx.Width), CInt(sizepx.Height))
         End Using
     End Function
+
+    Public Shared Function EffectStatic(source As IGraphicsEffectSource, resourceCreator As ICanvasResourceCreator, Optional blurAmount As Integer = 3) As IGraphicsEffectSource
+        Dim rect = CType(source, ICanvasImage).GetBounds(resourceCreator)
+        Using cac = New CanvasRenderTarget(CType(resourceCreator, ICanvasResourceCreatorWithDpi), New Size(rect.Width, rect.Height))
+            Dim sizepx = cac.SizeInPixels
+            Using ds = cac.CreateDrawingSession
+                Using blur = New Effects.GaussianBlurEffect With {.Source = source, .BlurAmount = blurAmount}
+                    ds.Clear(Windows.UI.Colors.Transparent)
+                    ds.DrawImage(blur)
+                End Using
+            End Using
+            Return CanvasBitmap.CreateFromColors(resourceCreator, cac.GetPixelColors, CInt(sizepx.Width), CInt(sizepx.Height))
+        End Using
+    End Function
 End Class
