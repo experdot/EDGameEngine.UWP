012102af4efcd88f63aad14da08fa89be588f84e
experdot <1143116306@qq.com>
2016-11-06 13:58:23 +0800

    添加Sprite模型
    一种包含贴图的游戏物体

---
+++ EDGameEngine.Core/GameObject/GameView/Sprite/SpriteView.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Numerics
+Imports EDGameEngine.Core
+Imports Microsoft.Graphics.Canvas
+Imports Windows.UI
+Public Class SpriteView
+    Inherits TypedGameView(Of Sprite)
+    Public Sub New(Target As Sprite)
+        MyBase.New(Target)
+    End Sub
+    Public Overrides Sub OnDraw(DrawingSession As CanvasDrawingSession)
+        If Target.Image IsNot Nothing Then
+            DrawingSession.DrawImage(CType(Target.Image, ICanvasImage))
+        End If
+    End Sub
+End Class
+
---
+++ EDGameEngine.Core/GameObject/GameVisual/Body/Sprite/Sprite.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.Graphics.Canvas
+''' <summary>
+''' 贴图
+''' </summary>
+Public Class Sprite
+    Inherits GameBody
+    Public Property Image As CanvasBitmap
+    Public Overrides Sub StartEx()
+
+    End Sub
+    Public Overrides Sub UpdateEx()
+
+    End Sub
+End Class
