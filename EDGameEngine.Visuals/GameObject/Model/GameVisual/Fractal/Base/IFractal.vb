b19ed731be1615c091adbf2338cafdbe93487770
experdot <1143116306@qq.com>
2016-11-19 00:06:18 +0800

    修复曼德布罗集的错误并加入NatureTree
    之前顶点坐标计算有误

---
+++ EDGameEngine.Visuals/GameObject/Model/GameVisual/Fractal/Base/IFractal.vb
@@ -0,0 +1,11 @@
+﻿Imports EDGameEngine.Core
+''' <summary>
+''' 分形
+''' </summary>
+Public Interface IFractal
+    Inherits IGameBody
+    ''' <summary>
+    ''' 顶点集合
+    ''' </summary>
+    Property Vertexs As List(Of ColorVertex）
+End Interface
---
+++ EDGameEngine.Visuals/GameObject/View/GameView/FractalView.vb
@@ -0,0 +1,19 @@
+﻿Imports EDGameEngine.Core
+Imports Microsoft.Graphics.Canvas
+''' <summary>
+''' 分形的视图
+''' </summary>
+Public Class FractalView
+    Inherits TypedGameView(Of IFractal)
+    Public Sub New(Target As IFractal)
+        MyBase.New(Target)
+    End Sub
+    Public Overrides Sub OnDraw(DrawingSession As CanvasDrawingSession)
+        If Target.Vertexs.Count > 0 Then
+            For Each SubVertex In Target.Vertexs
+                DrawingSession.DrawRectangle(SubVertex.Position.X, SubVertex.Position.Y, 1, 1, SubVertex.Color)
+            Next
+            Target.Vertexs.Clear()
+        End If
+    End Sub
+End Class
