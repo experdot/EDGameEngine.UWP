9bd5ba63fb72b97240873143898188507777c873
experdot <experdot@foxmail.com>
2017-07-09 16:49:48 +0800

    简化多窗体的创建
    简化代码实现

--- ExampleGallery/Common/Game/GameStarter.vb
+++ ExampleGallery/Common/Game/GameStarter.vb
@@ -1,40 +1,8 @@
-﻿Imports ExampleGallery.Windows10.UI.MultipleViews
-Imports Windows.UI.Core
-''' <summary>
+﻿''' <summary>
 ''' 游戏启动器
 ''' </summary>
 Public Class GameStarter
-    Shared SecondaryViewHelper As SecondaryViewHelper
-
     Public Shared Async Sub Start(Optional title As String = "")
-        Await CreateNewWindowAsync(GetType(Scenario5_Game), title)
+        Await MultiWindow.CreateNewWindowAsync(GetType(Scenario5_Game), title)
     End Sub
-
-    Private Shared Async Function CreateNewWindowAsync(sourcePageType As Type, Optional title As String = "") As Task
-        ' * CoreApplication.CreateNewView() - 创建一个新的 SecondaryView（只是新建一个 SecondaryView 实例，并不会显示出来）        
-        Dim createwindow = Sub()
-                               SecondaryViewHelper = SecondaryViewHelper.CreateForCurrentView()
-                               SecondaryViewHelper.Title = title
-                               SecondaryViewHelper.StartViewInUse()
-                               Dim frame = New Frame()
-                               frame.Navigate(sourcePageType, SecondaryViewHelper)
-                               Window.Current.Content = frame
-                               Window.Current.Activate()
-                               ' 这里通过 ApplicationView.GetForCurrentView() 获取到的是新开窗口的 ApplicationView 对象
-                               Dim secondaryView As ApplicationView = ApplicationView.GetForCurrentView()
-                               secondaryView.Title = title
-                           End Sub
-        Await Core.CoreApplication.CreateNewView().Dispatcher.RunAsync(CoreDispatcherPriority.Normal, createwindow)
-        Try
-            SecondaryViewHelper.StartViewInUse()
-            ApplicationViewSwitcher.DisableShowingMainViewOnActivation()
-            Dim viewShown = Await ApplicationViewSwitcher.TryShowAsStandaloneAsync(SecondaryViewHelper.Id, ViewSizePreference.[Default], ApplicationView.GetForCurrentView().Id, ViewSizePreference.[Default])
-            If Not viewShown Then
-                Debug.WriteLine("显示 SecondaryView 失败")
-            End If
-            SecondaryViewHelper.StopViewInUse()
-        Catch ex As Exception
-            Debug.WriteLine(ex.ToString())
-        End Try
-    End Function
 End Class
---
+++ ExampleGallery/Common/MultiWindow/MultiWindow.vb
@@ -0,0 +1,27 @@
+﻿Imports Windows.UI.Core
+''' <summary>
+''' 用于创建多窗体的对象
+''' </summary>
+Public Class MultiWindow
+    ''' <summary>
+    ''' 创建一个新窗体
+    ''' </summary>
+    Public Shared Async Function CreateNewWindowAsync(sourcePageType As Type, Optional title As String = "") As Task
+        Dim viewId As Integer
+        Dim createwindow = Sub()
+                               Dim frame = New Frame()
+                               frame.Navigate(sourcePageType)
+                               Window.Current.Content = frame
+                               Window.Current.Activate()
+                               Dim view = ApplicationView.GetForCurrentView()
+                               view.Title = title
+                               viewId = view.Id
+                           End Sub
+        Await Core.CoreApplication.CreateNewView().Dispatcher.RunAsync(CoreDispatcherPriority.Normal, createwindow)
+        Try
+            Await ApplicationViewSwitcher.TryShowAsStandaloneAsync(viewId)
+        Catch ex As Exception
+            Debug.WriteLine(ex.ToString())
+        End Try
+    End Function
+End Class
