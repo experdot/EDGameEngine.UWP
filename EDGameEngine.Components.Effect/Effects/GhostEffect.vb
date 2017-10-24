f80fb6b50ad89e40c06ceef35c8b152c9e7ebea1
experdot <experdot@foxmail.com>
2017-10-25 01:33:44 +0800

    调整残影效果实现方式
    重新支持残影透明度和偏移量

--- EDGameEngine.Components.Effect/Effects/GhostEffect.vb
+++ EDGameEngine.Components.Effect/Effects/GhostEffect.vb
@@ -9,30 +9,56 @@ Imports Windows.Graphics.Effects
 Public Class GhostEffect
     Inherits CanvasEffectBase
     ''' <summary>
-    ''' [Unused]获取或设置残影不透明度
+    ''' 获取或设置残影不透明度
     ''' </summary>
     Public Property Opacity As Single = 1.0F
+    ''' <summary>
+    ''' 获取或设置残影偏移向量
+    ''' </summary>
+    Public Property Offset As Vector2 = Vector2.Zero
+
+    'Public Overrides Function Effect(source As IGraphicsEffectSource, resourceCreator As ICanvasResourceCreator) As IGraphicsEffectSource
+    '    Static LastSource As IGraphicsEffectSource
+
+    '    Dim rect = Target.Scene.Rect
+    '    If LastSource Is Nothing Then
+    '        Dim render = New CanvasRenderTarget(resourceCreator, CInt(rect.Width), CInt(rect.Height))
+    '        Using ds = render.CreateDrawingSession
+    '            ds.Clear(Windows.UI.Colors.Transparent)
+    '        End Using
+    '        LastSource = render
+    '    End If
+    '    Dim drawingSession As CanvasDrawingSession = resourceCreator
+
+
+    '    Dim temp = New CanvasRenderTarget(resourceCreator, CInt(rect.Width), CInt(rect.Height))
+    '    Using ds = temp.CreateDrawingSession
+    '        ds.Clear(Windows.UI.Colors.Transparent)
+    '        ds.DrawImage(CType(LastSource, ICanvasImage), Vector2.Zero, rect, Opacity)
+    '        ds.DrawImage(CType(source, ICanvasImage))
+    '    End Using
+    '    LastSource = temp
+
+    '    'Return temp
+    '    Return CanvasBitmap.CreateFromColors(resourceCreator, temp.GetPixelColors, CInt(temp.Bounds.Width), CInt(temp.Bounds.Height))
+    'End Function
+
 
     Public Overrides Function Effect(source As IGraphicsEffectSource, resourceCreator As ICanvasResourceCreator) As IGraphicsEffectSource
-        Static LastSource As IGraphicsEffectSource
+        Static LastSource As CanvasBitmap
 
-        Dim rect = Target.Scene.Rect
-        If LastSource Is Nothing Then
-            Dim render = New CanvasRenderTarget(resourceCreator, CInt(rect.Width), CInt(rect.Height))
-            Using ds = render.CreateDrawingSession
-                ds.Clear(Windows.UI.Colors.Transparent)
-            End Using
-            LastSource = render
-        End If
+        Dim rect As Rect = Target.Scene.Rect
+        Dim cmdlist = New CanvasCommandList(resourceCreator)
 
-        Dim temp = New CanvasRenderTarget(resourceCreator, CInt(rect.Width), CInt(rect.Height))
-        Using ds = temp.CreateDrawingSession
+        Using ds = cmdlist.CreateDrawingSession
             ds.Clear(Windows.UI.Colors.Transparent)
-            ds.DrawImage(CType(LastSource, ICanvasImage), Vector2.Zero)
-            ds.DrawImage(CType(source, ICanvasImage))
+            If LastSource IsNot Nothing Then
+                ds.DrawImage(LastSource, Offset, rect, Opacity)
+            End If
+            ds.DrawImage(source)
         End Using
-        LastSource = temp
 
-        Return CanvasBitmap.CreateFromColors(resourceCreator, temp.GetPixelColors, CInt(temp.Bounds.Width), CInt(temp.Bounds.Height))
+        LastSource = BitmapCacheHelper.CacheImageClip(resourceCreator, cmdlist, rect)
+        Return cmdlist
     End Function
 End Class
