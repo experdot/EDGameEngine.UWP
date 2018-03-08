da39dd8d52f052e80f9c5cd8469cef0ccfc3ecf5
experdot <experdot@foxmail.com>
2018-03-08 20:47:07 +0800

    Feature: 添加指针跟随脚本
    物体跟随指针移动

---
+++ EDGameEngine.Components.Behavior/Behaviors/PointerFollowScript.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Numerics
+Imports EDGameEngine.Core
+''' <summary>
+''' 指针跟随脚本
+''' </summary>
+Public Class PointerFollowScript
+    Inherits BehaviorBase
+    Public Overrides Sub Start()
+        RemoveHandler Scene.Inputs.Mouse.MouseChanged, AddressOf MouseChanged
+        AddHandler Scene.Inputs.Mouse.MouseChanged, AddressOf MouseChanged
+    End Sub
+
+    Public Overrides Sub Update()
+
+    End Sub
+
+    Private Sub MouseChanged(loc As Vector2)
+        Me.Target.Transform.Translation = loc - Scene.Camera.Position
+    End Sub
+End Class
